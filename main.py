105dd2a852e432e28fb3e5fb3f74a5f7ef14de16
Prince Sanjivy <34624548+princesanjivy@users.noreply.github.com>
2020-07-14 16:54:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests, json, pymongo, random
+from PIL import Image, ImageDraw, ImageFont
+from textwrap import wrap
+from instapy_cli import client
+
+def toImage(quoteText, quoteAuthor):
+
+	font = ImageFont.truetype('RougeScript.ttf', 70)
+	im = Image.new("RGB", (1080, 1080), '#0d0f11') #resolution of 1080x1080
+	draw = ImageDraw.Draw(im)
+	quoteText = wrap(quoteText, 35)
+	quoteAuthor = '-' + quoteAuthor
+
+	#quoteText
+	upper_half = quoteText[: len(quoteText)//2]	#upper half of the text
+	y = len(upper_half)
+	for line in upper_half:
+	    draw.text( (540-(font.getsize(line)[0]/2), 540-81*y), line, fill="#eeeeee", font=font)
+	    y -= 1
+
+	lower_half = quoteText[len(quoteText)//2 :]	#lower half of the text
+	y = 0
+	for line in lower_half:
+	    draw.text((540-(font.getsize(line)[0]/2), 540+81*y), line, fill="#eeeeee", font=font)
+	    y += 1
+
+	#quoteAuthor
+	draw.text((540-(font.getsize(quoteAuthor)[0]/2)+100, 540+81*y), quoteAuthor, fill='#eeeeee', font=font)
+
+	#tag
+	font = ImageFont.truetype('RougeScript.ttf', 30)
+	draw.text((900, 1040), "@quotes.princebot", fill='grey', font=font)
+
+	im.save("quote_image.png", quality=95)	#saving the image
+
+	return "quote_image.png"
+
+def getQuote():
+
+	url = 'mongodb+srv://princesanijvy:QWErty@quotebot-lrswm.gcp.mongodb.net/test?retryWrites=true&w=majority'
+	client = pymongo.MongoClient(url)
+
+	db = client["quotes"]
+	categories = ["success", "love", "humor", "motivational", "inspirational",
+	 "truth", "life-lessons", "science", "happiness", "relationships"]
+
+	collections = db[random.choice(categories)]
+
+	data = collections.find_one({"is_posted" : False})
+	_id = data.get("_id")
+	collections.update_one({"_id" : _id}, {"$set" : {"is_posted" : True}})
+
+	return data.get("quoteText"), data.get("quoteAuthor")
